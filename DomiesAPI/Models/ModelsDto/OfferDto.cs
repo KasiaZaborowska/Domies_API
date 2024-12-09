179dd7eb676f0626d1f86d20f15c29de1f16a22d
Kasia <kasiazet000@gmail.com>
2024-12-09 21:56:16 +0100

    Added OfferDto

---
+++ DomiesAPI/Models/ModelsDto/OfferDto.cs
@@ -0,0 +1,27 @@
+﻿namespace DomiesAPI.Models.ModelsDto
+{
+    public class OfferDto
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+
+        public string Photo { get; set; }
+
+        public string Description { get; set; } = null!;
+
+        public string Host { get; set; } = null!;
+
+        public int AddressId { get; set; }
+
+        public DateTime? DateAdd { get; set; }
+        public string Country { get; set; } = null!;
+
+        public string City { get; set; } = null!;
+
+        public string Street { get; set; } = null!;
+
+        public string PostalCode { get; set; } = null!;
+
+    }
+}
