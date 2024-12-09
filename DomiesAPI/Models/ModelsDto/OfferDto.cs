f0226471e06ffd92cad8414dc8517dab40c8d420
Kasia <kasiazet000@gmail.com>
2024-12-09 22:34:32 +0100

    OfferDto update

--- DomiesAPI/Models/ModelsDto/OfferDto.cs
+++ DomiesAPI/Models/ModelsDto/OfferDto.cs
@@ -4,24 +4,24 @@
     {
         public int Id { get; set; }
 
-        public string Title { get; set; } = null!;
+        public string? Title { get; set; } = null!;
 
-        public string Photo { get; set; }
+        public string? Photo { get; set; }
 
-        public string Description { get; set; } = null!;
+        public string? Description { get; set; } = null!;
 
-        public string Host { get; set; } = null!;
+        public string? Host { get; set; } = null!;
 
-        public int AddressId { get; set; }
+        public int? AddressId { get; set; }
 
         public DateTime? DateAdd { get; set; }
-        public string Country { get; set; } = null!;
+        public string? Country { get; set; } = null!;
 
-        public string City { get; set; } = null!;
+        public string? City { get; set; } = null!;
 
-        public string Street { get; set; } = null!;
+        public string? Street { get; set; } = null!;
 
-        public string PostalCode { get; set; } = null!;
+        public string? PostalCode { get; set; } = null!;
 
     }
 }
