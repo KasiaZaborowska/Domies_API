e7431325cf55774c639b035afc424cb7bc4f19df
Kasia <kasiazet000@gmail.com>
2024-12-09 22:51:18 +0100

    Added LoginDto

---
+++ DomiesAPI/Models/ModelsDto/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DomiesAPI.Models.ModelsDto
+{
+    public class LoginDto
+    {
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+    }
+}
