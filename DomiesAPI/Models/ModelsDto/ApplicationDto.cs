dc8a6e21e4f33e31652221b6d4e2221bb867815b
Kasia <kasiazet000@gmail.com>
2024-12-22 02:10:01 +0100

    ApplicationDto added

---
+++ DomiesAPI/Models/ModelsDto/ApplicationDto.cs
@@ -0,0 +1,21 @@
+﻿namespace DomiesAPI.Models.ModelsDto
+{
+    public class ApplicationDto
+    {
+        public int Id { get; set; }
+
+        public DateTime DateStart { get; set; }
+
+        public DateTime DateEnd { get; set; }
+
+        public int OfferId { get; set; }
+
+        public string ToUser { get; set; } = null!;
+
+        public DateTime? ApplicationDateAdd { get; set; }
+
+        public virtual ICollection<Opinion> Opinions { get; set; } = new List<Opinion>();
+
+        public virtual User ToUserNavigation { get; set; } = null!;
+    }
+}
