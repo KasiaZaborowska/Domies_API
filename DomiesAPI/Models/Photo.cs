9c3f154368c8c7c225a1518ca3a914a3aed3ee74
Kasia <kasiazet000@gmail.com>
2025-01-06 21:38:41 +0100

    Photo Model added

---
+++ DomiesAPI/Models/Photo.cs
@@ -0,0 +1,17 @@
+﻿namespace DomiesAPI.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Extension { get; set; } = null!;
+
+        public string Type { get; set; } = null!;
+
+        public byte[]? BinaryData { get; set; }
+
+        public virtual ICollection<Offer> Offers { get; set; } = new List<Offer>();
+    }
+}
