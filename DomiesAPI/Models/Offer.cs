0108fa0ba07cbb738dde2f25022d92a41e58042b
Kasia <kasiazet000@gmail.com>
2025-01-06 21:38:26 +0100

    Offer Model - 'photo' update

--- DomiesAPI/Models/Offer.cs
+++ DomiesAPI/Models/Offer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DomiesAPI.Models;
 
@@ -9,8 +10,6 @@ public partial class Offer
 
     public string Title { get; set; } = null!;
 
-    public string Photo { get; set; }
-
     public string Description { get; set; } = null!;
 
     public string Host { get; set; } = null!;
@@ -18,11 +17,18 @@ public partial class Offer
     public int AddressId { get; set; }
 
     public DateTime? DateAdd { get; set; }
+    public decimal? Price { get; set; }
+
+    public int? PhotoId { get; set; }
 
     public virtual Address Address { get; set; } = null!;
+    public virtual Photo? Photo { get; set; } = null!;
     public virtual ICollection<Application> Applications { get; set; } = new List<Application>();
 
 
 
     public virtual User HostNavigation { get; set; } = null!;
+    //public virtual Photo? PhotoNavigation { get; set; }
+
+    public virtual ICollection<OfferAnimalType> OfferAnimalTypes { get; set; } = new List<OfferAnimalType>();
 }
