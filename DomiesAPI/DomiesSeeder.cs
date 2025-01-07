3d3d70a7fa4cdf1d472d3163cf96dbf4a4837356
Kasia <kasiazet000@gmail.com>
2025-01-08 00:31:43 +0100

    DomiesSeeder update

--- DomiesAPI/DomiesSeeder.cs
+++ DomiesAPI/DomiesSeeder.cs
@@ -14,48 +14,48 @@ namespace DomiesAPI
 
         public void Seed()
         {
-            if (_context.Database.CanConnect())
-            {
-                //var pendingMigrations = _dbContext.Database.GetPendingMigrations();
-                //if (pendingMigrations != null && pendingMigrations.Any())   // czy tab z migracjami jest pusta
-                //{
-                //    _dbContext.Database.Migrate();
-                //}
+            //if (_context.Database.CanConnect())
+            //{
+            //    //var pendingMigrations = _dbContext.Database.GetPendingMigrations();
+            //    //if (pendingMigrations != null && pendingMigrations.Any())   // czy tab z migracjami jest pusta
+            //    //{
+            //    //    _dbContext.Database.Migrate();
+            //    //}
 
-                if (!_context.Roles.Any())
-                {
-                    var roles = GetRoles();
-                    _context.Roles.AddRange(roles);
-                    _context.SaveChanges();
-                }
+            //    if (!_context.Roles.Any())
+            //    {
+            //        var roles = GetRoles();
+            //        _context.Roles.AddRange(roles);
+            //        _context.SaveChanges();
+            //    }
 
-                if (!_context.Addresses.Any())
-                {
-                    var address = GetAddress();
-                    _context.Addresses.AddRange(address);
-                    _context.SaveChanges();
-                }
+            //    if (!_context.Addresses.Any())
+            //    {
+            //        var address = GetAddress();
+            //        _context.Addresses.AddRange(address);
+            //        _context.SaveChanges();
+            //    }
 
-                if (!_context.Users.Any())
-                {
-                    var users = GetUsers();
-                    _context.Users.AddRange(users);
-                    _context.SaveChanges();
-                }
+            //    if (!_context.Users.Any())
+            //    {
+            //        var users = GetUsers();
+            //        _context.Users.AddRange(users);
+            //        _context.SaveChanges();
+            //    }
 
 
-                if (!_context.Offers.Any())
-                {
-                    var offers = GetOffers();
-                    _context.Offers.AddRange(offers);
-                    _context.SaveChanges();
-                }
+            //    if (!_context.Offers.Any())
+            //    {
+            //        var offers = GetOffers();
+            //        _context.Offers.AddRange(offers);
+            //        _context.SaveChanges();
+            //    }
 
-            }
-            else
-            {
-                Console.WriteLine("Roles already exists.");
-            }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Roles already exists.");
+            //}
         }
 
         private IEnumerable<Role> GetRoles()
@@ -167,7 +167,6 @@ namespace DomiesAPI
                 new Offer()
                 {
                     Title = "Kasia",
-                    Photo = "/Images/offer_1.jpg",
                     Description ="Cześć! Jestem osobą, która uczy się zaocznie i pracuje zdalnie, co daje mi możliwość poświęcenia dużo czasu na opiekę nad Twoim pupilem. Mam już duże doświadczenie w opiece nad zwierzętami, ponieważ mam dwa kotki i pieska, które są spokojne i przyjaźnie nastawione do innych zwierząt. Mieszkam w domu z dużym ogródkiem, więc Twój pupil będzie miał dużo przestrzeni do zabawy i wypoczynku. Okolica jest cicha i spokojna, a w pobliżu znajdują się liczne parki oraz ścieżki leśne, które chętnie pokażę Twojemu zwierzakowi.\r\n\r\nZależy mi na tym, aby Twój pupil czuł się u mnie jak w domu, więc zapewnię mu komfort i troskliwą opiekę. Wierzę, że będzie chętnie wracał!\r\n\r\nZajmuję się także zwierzakami starszymi i chorymi, które wymagają specjalnej opieki, podawania leków czy wykonywania innych procedur pielęgnacyjnych. Mam duże serce i pełne zrozumienie dla ich potrzeb, traktując każdego pupila jak członka rodziny. Serdecznie zapraszam do kontaktu!",
                     AddressId = 2011,
                     Host = "email@gmail.com",
@@ -177,7 +176,6 @@ namespace DomiesAPI
                 new Offer()
                 {
                     Title = "Magdalena",
-                    Photo = "/Images/offer_2.jpg",
                     Description ="Cześć! Jestem osobą, która uczy się zaocznie i pracuje zdalnie, co daje mi możliwość poświęcenia dużo czasu na opiekę nad Twoim pupilem. Mam już duże doświadczenie w opiece nad zwierzętami, ponieważ mam dwa kotki i pieska, które są spokojne i przyjaźnie nastawione do innych zwierząt. Mieszkam w domu z dużym ogródkiem, więc Twój pupil będzie miał dużo przestrzeni do zabawy i wypoczynku. Okolica jest cicha i spokojna, a w pobliżu znajdują się liczne parki oraz ścieżki leśne, które chętnie pokażę Twojemu zwierzakowi.\r\n\r\nZależy mi na tym, aby Twój pupil czuł się u mnie jak w domu, więc zapewnię mu komfort i troskliwą opiekę. Wierzę, że będzie chętnie wracał!\r\n\r\nZajmuję się także zwierzakami starszymi i chorymi, które wymagają specjalnej opieki, podawania leków czy wykonywania innych procedur pielęgnacyjnych. Mam duże serce i pełne zrozumienie dla ich potrzeb, traktując każdego pupila jak członka rodziny. Serdecznie zapraszam do kontaktu!",
                     AddressId = 2012,
                     Host = "email@gmail.com",
@@ -187,7 +185,6 @@ namespace DomiesAPI
                 new Offer()
                 {
                     Title = "Piotrek",
-                    Photo = "/Images/offer_3.jpg",
                     Description ="Cześć! Jestem osobą, która uczy się zaocznie i pracuje zdalnie, co daje mi możliwość poświęcenia dużo czasu na opiekę nad Twoim pupilem. Mam już duże doświadczenie w opiece nad zwierzętami, ponieważ mam dwa kotki i pieska, które są spokojne i przyjaźnie nastawione do innych zwierząt. Mieszkam w domu z dużym ogródkiem, więc Twój pupil będzie miał dużo przestrzeni do zabawy i wypoczynku. Okolica jest cicha i spokojna, a w pobliżu znajdują się liczne parki oraz ścieżki leśne, które chętnie pokażę Twojemu zwierzakowi.\r\n\r\nZależy mi na tym, aby Twój pupil czuł się u mnie jak w domu, więc zapewnię mu komfort i troskliwą opiekę. Wierzę, że będzie chętnie wracał!\r\n\r\nZajmuję się także zwierzakami starszymi i chorymi, które wymagają specjalnej opieki, podawania leków czy wykonywania innych procedur pielęgnacyjnych. Mam duże serce i pełne zrozumienie dla ich potrzeb, traktując każdego pupila jak członka rodziny. Serdecznie zapraszam do kontaktu!",
                     AddressId = 2013,
                     Host = "email@gmail.com",
@@ -197,7 +194,6 @@ namespace DomiesAPI
                 new Offer()
                 {
                     Title = "Kasia",
-                    Photo = "/Images/offer_4.jpg",
                     Description ="Cześć! Jestem osobą, która uczy się zaocznie i pracuje zdalnie, co daje mi możliwość poświęcenia dużo czasu na opiekę nad Twoim pupilem. Mam już duże doświadczenie w opiece nad zwierzętami, ponieważ mam dwa kotki i pieska, które są spokojne i przyjaźnie nastawione do innych zwierząt. Mieszkam w domu z dużym ogródkiem, więc Twój pupil będzie miał dużo przestrzeni do zabawy i wypoczynku. Okolica jest cicha i spokojna, a w pobliżu znajdują się liczne parki oraz ścieżki leśne, które chętnie pokażę Twojemu zwierzakowi.\r\n\r\nZależy mi na tym, aby Twój pupil czuł się u mnie jak w domu, więc zapewnię mu komfort i troskliwą opiekę. Wierzę, że będzie chętnie wracał!\r\n\r\nZajmuję się także zwierzakami starszymi i chorymi, które wymagają specjalnej opieki, podawania leków czy wykonywania innych procedur pielęgnacyjnych. Mam duże serce i pełne zrozumienie dla ich potrzeb, traktując każdego pupila jak członka rodziny. Serdecznie zapraszam do kontaktu!",
                     AddressId = 2014,
                     Host = "email@gmail.com",
@@ -207,7 +203,6 @@ namespace DomiesAPI
                 new Offer()
                 {
                     Title = "Magda",
-                    Photo = "/Images/offer_5.jpg",
                     Description ="Cześć! Jestem osobą, która uczy się zaocznie i pracuje zdalnie, co daje mi możliwość poświęcenia dużo czasu na opiekę nad Twoim pupilem. Mam już duże doświadczenie w opiece nad zwierzętami, ponieważ mam dwa kotki i pieska, które są spokojne i przyjaźnie nastawione do innych zwierząt. Mieszkam w domu z dużym ogródkiem, więc Twój pupil będzie miał dużo przestrzeni do zabawy i wypoczynku. Okolica jest cicha i spokojna, a w pobliżu znajdują się liczne parki oraz ścieżki leśne, które chętnie pokażę Twojemu zwierzakowi.\r\n\r\nZależy mi na tym, aby Twój pupil czuł się u mnie jak w domu, więc zapewnię mu komfort i troskliwą opiekę. Wierzę, że będzie chętnie wracał!\r\n\r\nZajmuję się także zwierzakami starszymi i chorymi, które wymagają specjalnej opieki, podawania leków czy wykonywania innych procedur pielęgnacyjnych. Mam duże serce i pełne zrozumienie dla ich potrzeb, traktując każdego pupila jak członka rodziny. Serdecznie zapraszam do kontaktu!",
                     AddressId = 2015,
                     Host = "email@gmail.com",
@@ -217,7 +212,6 @@ namespace DomiesAPI
                 new Offer()
                 {
                     Title = "Karol",
-                    Photo = "/Images/offer_6.jpg",
                     Description ="Cześć! Jestem osobą, która uczy się zaocznie i pracuje zdalnie, co daje mi możliwość poświęcenia dużo czasu na opiekę nad Twoim pupilem. Mam już duże doświadczenie w opiece nad zwierzętami, ponieważ mam dwa kotki i pieska, które są spokojne i przyjaźnie nastawione do innych zwierząt. Mieszkam w domu z dużym ogródkiem, więc Twój pupil będzie miał dużo przestrzeni do zabawy i wypoczynku. Okolica jest cicha i spokojna, a w pobliżu znajdują się liczne parki oraz ścieżki leśne, które chętnie pokażę Twojemu zwierzakowi.\r\n\r\nZależy mi na tym, aby Twój pupil czuł się u mnie jak w domu, więc zapewnię mu komfort i troskliwą opiekę. Wierzę, że będzie chętnie wracał!\r\n\r\nZajmuję się także zwierzakami starszymi i chorymi, które wymagają specjalnej opieki, podawania leków czy wykonywania innych procedur pielęgnacyjnych. Mam duże serce i pełne zrozumienie dla ich potrzeb, traktując każdego pupila jak członka rodziny. Serdecznie zapraszam do kontaktu!",
                     AddressId = 2016,
                     Host = "email@gmail.com",
@@ -227,7 +221,6 @@ namespace DomiesAPI
                 new Offer()
                 {
                     Title = "Krzysztof",
-                    Photo = "/Images/offer_7.jpg",
                     Description ="Cześć! Jestem osobą, która uczy się zaocznie i pracuje zdalnie, co daje mi możliwość poświęcenia dużo czasu na opiekę nad Twoim pupilem. Mam już duże doświadczenie w opiece nad zwierzętami, ponieważ mam dwa kotki i pieska, które są spokojne i przyjaźnie nastawione do innych zwierząt. Mieszkam w domu z dużym ogródkiem, więc Twój pupil będzie miał dużo przestrzeni do zabawy i wypoczynku. Okolica jest cicha i spokojna, a w pobliżu znajdują się liczne parki oraz ścieżki leśne, które chętnie pokażę Twojemu zwierzakowi.\r\n\r\nZależy mi na tym, aby Twój pupil czuł się u mnie jak w domu, więc zapewnię mu komfort i troskliwą opiekę. Wierzę, że będzie chętnie wracał!\r\n\r\nZajmuję się także zwierzakami starszymi i chorymi, które wymagają specjalnej opieki, podawania leków czy wykonywania innych procedur pielęgnacyjnych. Mam duże serce i pełne zrozumienie dla ich potrzeb, traktując każdego pupila jak członka rodziny. Serdecznie zapraszam do kontaktu!",
                     AddressId = 2017,
                     Host = "email@gmail.com",
@@ -237,7 +230,6 @@ namespace DomiesAPI
                 new Offer()
                 {
                     Title = "Ola",
-                    Photo = "/Images/offer_8.jpg",
                     Description ="Cześć! Jestem osobą, która uczy się zaocznie i pracuje zdalnie, co daje mi możliwość poświęcenia dużo czasu na opiekę nad Twoim pupilem. Mam już duże doświadczenie w opiece nad zwierzętami, ponieważ mam dwa kotki i pieska, które są spokojne i przyjaźnie nastawione do innych zwierząt. Mieszkam w domu z dużym ogródkiem, więc Twój pupil będzie miał dużo przestrzeni do zabawy i wypoczynku. Okolica jest cicha i spokojna, a w pobliżu znajdują się liczne parki oraz ścieżki leśne, które chętnie pokażę Twojemu zwierzakowi.\r\n\r\nZależy mi na tym, aby Twój pupil czuł się u mnie jak w domu, więc zapewnię mu komfort i troskliwą opiekę. Wierzę, że będzie chętnie wracał!\r\n\r\nZajmuję się także zwierzakami starszymi i chorymi, które wymagają specjalnej opieki, podawania leków czy wykonywania innych procedur pielęgnacyjnych. Mam duże serce i pełne zrozumienie dla ich potrzeb, traktując każdego pupila jak członka rodziny. Serdecznie zapraszam do kontaktu!",
                     AddressId = 2018,
                     Host = "email@gmail.com",
@@ -247,7 +239,6 @@ namespace DomiesAPI
                 new Offer()
                 {
                     Title = "Kamila",
-                    Photo = "/Images/offer_9.jpg",
                     Description ="Cześć! Jestem osobą, która uczy się zaocznie i pracuje zdalnie, co daje mi możliwość poświęcenia dużo czasu na opiekę nad Twoim pupilem. Mam już duże doświadczenie w opiece nad zwierzętami, ponieważ mam dwa kotki i pieska, które są spokojne i przyjaźnie nastawione do innych zwierząt. Mieszkam w domu z dużym ogródkiem, więc Twój pupil będzie miał dużo przestrzeni do zabawy i wypoczynku. Okolica jest cicha i spokojna, a w pobliżu znajdują się liczne parki oraz ścieżki leśne, które chętnie pokażę Twojemu zwierzakowi.\r\n\r\nZależy mi na tym, aby Twój pupil czuł się u mnie jak w domu, więc zapewnię mu komfort i troskliwą opiekę. Wierzę, że będzie chętnie wracał!\r\n\r\nZajmuję się także zwierzakami starszymi i chorymi, które wymagają specjalnej opieki, podawania leków czy wykonywania innych procedur pielęgnacyjnych. Mam duże serce i pełne zrozumienie dla ich potrzeb, traktując każdego pupila jak członka rodziny. Serdecznie zapraszam do kontaktu!",
                     AddressId = 2019,
                     Host = "email@gmail.com",
