45b9852301e0ca950605d52082e07f8c2ef38764
Kasia <kasiazet000@gmail.com>
2025-01-12 23:52:54 +0100

    Animal Dto added

---
+++ DomiesAPI/Models/ModelsDto/AnimalDto.cs
@@ -0,0 +1,16 @@
+﻿namespace DomiesAPI.Models.ModelsDto
+{
+    public class AnimalDto
+    {
+        public int Id { get; set; }
+
+        public string PetName { get; set; } = null!;
+
+        public string SpecificDescription { get; set; } = null!;
+
+        public string Owner { get; set; } = null!;
+
+        public int AnimalType { get; set; }
+
+    }
+}
