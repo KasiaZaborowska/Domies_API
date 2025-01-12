8a00f68499c8304030d2790b6292a7c42ce431b8
Kasia <kasiazet000@gmail.com>
2025-01-12 23:52:34 +0100

    OfferDtoRead added

---
+++ DomiesAPI/Models/ModelsDto/OfferDtoRead.cs
@@ -0,0 +1,32 @@
+﻿namespace DomiesAPI.Models.ModelsDto
+{
+    public class OfferDtoRead
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; } = null!;
+
+        public string? Description { get; set; } = null!;
+
+        public string? Host { get; set; } = null!;
+
+        public int? AddressId { get; set; }
+
+        public DateTime? DateAdd { get; set; }
+        
+        public string? Country { get; set; } = null!;
+
+        public string? City { get; set; } = null!;
+
+        public string? Street { get; set; } = null!;
+
+        public string? PostalCode { get; set; } = null!;
+        public decimal? Price { get; set; }
+
+        //public List<AnimalTypeDto>? OfferAnimalTypes { get; set; }
+        public string OfferAnimalTypes { get; set; } = null!;
+
+        public IFormFile? File { get; set; }
+        public string? Photo { get; set; }
+    }
+}
