0c795442f7f4151bef6ee9a16fda497684359729
Kasia <kasiazet000@gmail.com>
2025-01-15 23:11:42 +0100

    Application Animals create

---
+++ DomiesAPI/Models/ApplicationsAnimals.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace DomiesAPI.Models
+{
+    public class ApplicationsAnimals
+    {
+        public int? ApplicationId { get; set; }
+
+        public int? AnimalId { get; set; }
+
+        public virtual Application? Application { get; set; }
+
+        public virtual Animal? Animal { get; set; }
+    }
+}
