ef7065d02f95d5b4e58bbe4442ca64524b475951
Kasia <kasiazet000@gmail.com>
2025-01-26 22:45:09 +0100

    small update

--- DomiesAPI/Models/ModelsDto/UserDto.cs
+++ DomiesAPI/Models/ModelsDto/UserDto.cs
@@ -11,6 +11,7 @@
         public string Password { get; set; } = null!;
 
         public int RoleId { get; set; } = 1;
+        public string? RoleName { get; set; }
 
         public DateTime? DateAdd { get; set; }
     }
