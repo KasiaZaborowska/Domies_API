492fbae9bab75cfaedff7688fa563c60d9021297
Kasia <kasiazet000@gmail.com>
2025-02-03 23:44:05 +0100

    Add EmailService and verificationToken

--- DomiesAPI/Controllers/UserAccountController.cs
+++ DomiesAPI/Controllers/UserAccountController.cs
@@ -10,10 +10,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using System.Drawing;
 using System.IdentityModel.Tokens.Jwt;
 using System.Net;
 using System.Security.Claims;
 using System.Text;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace DomiesAPI.Controllers
 {
@@ -25,12 +27,13 @@ namespace DomiesAPI.Controllers
         //private readonly DomiesContext _context;
         //private ApiResponse _response;
         private IUserAccountService _userAccountService;
-
-        public UserAccountController(IUserAccountService userAccountService)
+        private IEmailService _emailService;
+        public UserAccountController(IUserAccountService userAccountService, IEmailService emailService)
         {
             //_context = context;
             //_response = apiResponse;
             _userAccountService = userAccountService;
+            _emailService = emailService;
         }
 
         [HttpPost("register")]
@@ -38,13 +41,124 @@ namespace DomiesAPI.Controllers
         {
             if (ModelState.IsValid)
             {
-                string result = await _userAccountService.RegisterUser(userdto);
+                var verificationToken = Guid.NewGuid().ToString();
+                string result = await _userAccountService.RegisterUser(userdto, verificationToken);
+
                 if (result == "Użytkownik z danym mailem już istnieje.")
                 {
                     return BadRequest(new { message = result });
                 }
                 else
                 {
+                    var verificationLink = $"http://localhost:3000/verify?token={verificationToken}";
+                    Console.WriteLine("Rejestracja w toku..");
+                    var emailSubject = "Zweryfikuj swój adres e-mail w serwisie Domies";
+                    var emailBody = $@"
+                            <!DOCTYPE html>
+                    <html lang='pl'>
+                    <head>
+                      <meta charset='UTF-8'>
+                      <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+                      <title>Weryfikacja konta</title>
+                      <style>
+                        body {{
+                          font-family: Arial, sans-serif;
+                          line-height: 1.6;
+                          color: #333333;
+                          margin: 0px;
+                          padding: 0px;
+                          width: 91%;
+                          max-width: 610px;
+                          background-color: #f9f9f9;
+                          border: 2px solid #e0e0e0;
+                          border-radius: 3px;
+                        }}
+                        .container {{
+                          width: 90%;
+                          max-width: 600px;
+                          margin: 20px auto;
+                          border: 2px solid #e0e0e0;
+                          border-radius: 16px;
+                          padding: 20px;
+                          font-size: 17px;
+                          background-color: #ffffff;
+                        }}
+                        .header {{
+                          text-align: center;
+                          background-color: #f3ecdb;
+                          border: 1px solid #c3b091;
+                          color: black;
+                          padding: 20px 0;
+                          border-top-left-radius: 10px;
+                          border-top-right-radius: 10px;
+                        }}
+                        .content {{
+                          text-align: center;
+                          margin: 10px 0px;
+                        }}
+                        .button {{
+                          display: inline-block;
+                          border: 1px solid #c3b091;
+                          background-color: #f3ecdb;
+                          color: black;
+                          text-decoration: none;
+                          padding: 10px 20px;
+                          font-size: 19px;
+                          border-radius: 5px;
+                          margin-top: 20px;
+                          margin-bottom: 10px;
+                        }}
+                        .button:hover {{
+                          background-color: #c3b091;
+                        }}
+                        .advice {{
+                          background-color: #f9f9f9;
+                          border: 1px solid #d1d1d1;
+                          text-align: left;
+                          color: #333;
+                          padding: 15px;
+                          border-radius: 8px;
+                          margin-bottom: 20px;
+                        }}
+                        .footer {{
+                          text-align: center;
+                          margin-top: 30px;
+                          font-size: 12px;
+                          color: #777777;
+                        }}
+                      </style>
+                    </head>
+                    <body>
+                      <div class='container'>
+                        <div class='header'>
+                          <h1>Witamy w [Nazwa Twojej Aplikacji]!</h1>
+                        </div>
+                        <div class='content'>
+                          <p>Witaj,</p>
+                          <p>Dziękujemy za utworzenie konta w <strong>[Nazwa Twojej Aplikacji]</strong>. Aby zakończyć proces rejestracji, kliknij poniższy przycisk, aby zweryfikować swój adres e-mail:</p>
+                          <a href='{{verificationLink}}' class='button'>Zweryfikuj swój e-mail</a>
+                          <p>Jeśli nie zakładałeś/aś konta w naszej aplikacji, zignoruj tę wiadomość.</p>
+                        </div>
+                        <div>
+                          <br>
+                          <div class='advice'>
+                            <h4 class='content'>Ważne wskazówki dotyczące bezpieczeństwa:</h4>
+                            <ol>
+                              <li>Zawsze trzymaj dane do Twojego konta w bezpiecznym miejscu.</li>
+                              <li>Nigdy nie ujawniaj nikomu swojego loginu i hasła.</li>
+                              <li>Regularnie zmieniaj swoje hasło.</li>
+                              <li>Jeśli podejrzewasz, że ktoś używa Twojego konta nielegalnie, prosimy o natychmiastowe poinformowanie nas o tym.</li>
+                            </ol>
+                          </div>
+                        </div>
+                        <div class='footer'>
+                          <p>© 2025 [Nazwa Twojej Aplikacji]. Wszelkie prawa zastrzeżone.</p>
+                        </div>
+                      </div>
+                    </body>
+                    </html>"; 
+                    await _emailService.SendVerificationEmailAsync(userdto.Email, emailSubject, emailBody);
+
                     return Ok(new { token = result, user = userdto.Email });
                 }
             }
@@ -57,6 +171,20 @@ namespace DomiesAPI.Controllers
 
             //return BadRequest(new { message = "Wystąpił błąd w rejestracji." });
         }
+
+        //[HttpPost("verify")]
+        //public async Task<IActionResult> VerifyUserEmail([FromBody] string token)
+        //{
+        //    var user = await _context.Users.FirstOrDefaultAsync(u => u.VerificationToken == request.Token);
+        //    if (user == null)
+        //        return BadRequest("Niepoprawny token");
+
+        //    user.IsVerified = true;
+        //    user.VerificationToken = null; // Token nie jest już potrzebny
+        //    await _context.SaveChangesAsync();
+
+        //    return Ok("Twoje konto zostało zweryfikowane!");
+        //}
 
 
         [HttpPost("login")]
