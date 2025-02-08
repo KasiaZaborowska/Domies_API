69cf6f1ceacb035b01e2fa6140b634ff98cf3c49
Kasia <kasiazet000@gmail.com>
2025-02-08 23:50:15 +0100

    ErrorMessage created

--- DomiesAPI/Models/ModelsDto/AnimalDto.cs
+++ DomiesAPI/Models/ModelsDto/AnimalDto.cs
@@ -1,15 +1,20 @@
-﻿namespace DomiesAPI.Models.ModelsDto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DomiesAPI.Models.ModelsDto
 {
     public class AnimalDto
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Imię zwierzaka jest wymagane.")]
         public string PetName { get; set; } = null!;
 
+        [Required(ErrorMessage = "Szczegółowy opis zwierzaka jest wymagany.")]
         public string SpecificDescription { get; set; } = null!;
 
         public string? Owner { get; set; } = null!;
 
+        [Required(ErrorMessage = "Typ zwierzaka jest wymagany.")]
         public int AnimalType { get; set; }
         public string? Type { get; set; }
 
