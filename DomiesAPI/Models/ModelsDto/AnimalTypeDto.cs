a08c5ffa0b5e3be84b011cf2feefd801871cf399
Kasia <kasiazet000@gmail.com>
2025-02-08 23:50:27 +0100

    ErrorMessage created

--- DomiesAPI/Models/ModelsDto/AnimalTypeDto.cs
+++ DomiesAPI/Models/ModelsDto/AnimalTypeDto.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DomiesAPI.Models.ModelsDto
 {
     public class AnimalTypeDto
     {
         public int AnimalTypeId { get; set; }
+        [Required(ErrorMessage = "Typ zwierzęcia jest wymagany.")]
         public string Type { get; set; }
 
         //public virtual ICollection<OfferAnimalType> OfferAnimalTypes { get; set; } = new List<OfferAnimalType>();
