e9ed51b9135fdcb49c4993864c61fb2c2a5d2527
Kasia <kasiazet000@gmail.com>
2025-02-08 23:49:59 +0100

    ModelState.IsValid remove

--- DomiesAPI/Controllers/UserAccountController.cs
+++ DomiesAPI/Controllers/UserAccountController.cs
@@ -38,8 +38,8 @@ namespace DomiesAPI.Controllers
 
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] UserDto userdto)
-        {
-            if (ModelState.IsValid)
+        {       
+            try
             {
                 var verificationToken = Guid.NewGuid().ToString();
                 string result = await _userAccountService.RegisterUser(userdto, verificationToken);
@@ -54,157 +54,160 @@ namespace DomiesAPI.Controllers
                     Console.WriteLine("Rejestracja w toku..");
                     var emailSubject = "Zweryfikuj swój adres e-mail w serwisie Domies";
                     var emailBody = $@"
-                            <!DOCTYPE html>
-                    <html lang='pl'>
-                    <head>
-                      <meta charset='UTF-8'>
-                      <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-                      <title>Weryfikacja konta</title>
-                      <style>
-                        body {{
-                          font-family: Arial, sans-serif;
-                          line-height: 1.6;
-                          color: #333333;
-                          margin: 0px;
-                          padding: 0px;
-                          width: 91%;
-                          max-width: 610px;
-                          background-color: #f9f9f9;
-                          border: 2px solid #e0e0e0;
-                          border-radius: 3px;
-                        }}
-                        .container {{
-                          width: 90%;
-                          max-width: 600px;
-                          margin: 20px auto;
-                          border: 2px solid #e0e0e0;
-                          border-radius: 16px;
-                          padding: 20px;
-                          font-size: 17px;
-                          background-color: #ffffff;
-                        }}
-                        .header {{
-                          text-align: center;
-                          background-color: #f3ecdb;
-                          border: 1px solid #c3b091;
-                          color: black;
-                          padding: 20px 0;
-                          border-top-left-radius: 10px;
-                          border-top-right-radius: 10px;
-                        }}
-                        .content {{
-                          text-align: center;
-                          margin: 10px 0px;
-                        }}
-                        .button {{
-                          display: inline-block;
-                          border: 1px solid #c3b091;
-                          background-color: #f3ecdb;
-                          color: black;
-                          text-decoration: none;
-                          padding: 10px 20px;
-                          font-size: 19px;
-                          border-radius: 5px;
-                          margin-top: 20px;
-                          margin-bottom: 10px;
-                        }}
-                        .button:hover {{
-                          background-color: #c3b091;
-                        }}
-                        .advice {{
-                          background-color: #f9f9f9;
-                          border: 1px solid #d1d1d1;
-                          text-align: left;
-                          color: #333;
-                          padding: 15px;
-                          border-radius: 8px;
-                          margin-bottom: 20px;
-                        }}
-                        .footer {{
-                          text-align: center;
-                          margin-top: 30px;
-                          font-size: 12px;
-                          color: #777777;
-                        }}
-                      </style>
-                    </head>
-                    <body>
-                      <div class='container'>
-                        <div class='header'>
-                          <h1>Witamy w [Nazwa Twojej Aplikacji]!</h1>
+                        <!DOCTYPE html>
+                <html lang='pl'>
+                <head>
+                    <meta charset='UTF-8'>
+                    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+                    <title>Weryfikacja konta</title>
+                    <style>
+                    body {{
+                        font-family: Arial, sans-serif;
+                        line-height: 1.6;
+                        color: #333333;
+                        margin: 0px;
+                        padding: 0px;
+                        width: 91%;
+                        max-width: 610px;
+                        background-color: #f9f9f9;
+                        border: 2px solid #e0e0e0;
+                        border-radius: 3px;
+                    }}
+                    .container {{
+                        width: 90%;
+                        max-width: 600px;
+                        margin: 20px auto;
+                        border: 2px solid #e0e0e0;
+                        border-radius: 16px;
+                        padding: 20px;
+                        font-size: 17px;
+                        background-color: #ffffff;
+                    }}
+                    .header {{
+                        text-align: center;
+                        background-color: #f3ecdb;
+                        border: 1px solid #c3b091;
+                        color: black;
+                        padding: 20px 0;
+                        border-top-left-radius: 10px;
+                        border-top-right-radius: 10px;
+                    }}
+                    .content {{
+                        text-align: center;
+                        color: black;
+                        margin: 10px 0px;
+                    }}
+                    .button {{
+                        display: inline-block;
+                        border: 1px solid #c3b091;
+                        background-color: #f3ecdb;
+                        color: black;
+                        text-decoration: none;
+                        padding: 10px 20px;
+                        font-size: 19px;
+                        border-radius: 5px;
+                        margin-top: 20px;
+                        margin-bottom: 10px;
+                    }}
+                    .button:hover {{
+                        background-color: #c3b091;
+                    }}
+                    .advice {{
+                        background-color: #f9f9f9;
+                        border: 1px solid #d1d1d1;
+                        text-align: left;
+                        color: #333;
+                        padding: 15px;
+                        border-radius: 8px;
+                        margin-bottom: 20px;
+                    }}
+                    .footer {{
+                        text-align: center;
+                        margin-top: 30px;
+                        font-size: 12px;
+                        color: #777777;
+                    }}
+                    </style>
+                </head>
+                <body>
+                    <div class='container'>
+                    <div class='header'>
+                        <h1>Witamy w aplikacji Domies!</h1>
+                    </div>
+                    <div class='content'>
+                        <p>Witaj,</p>
+                        <p>Dziękujemy za utworzenie konta w <strong>Domies</strong>. Aby zakończyć proces rejestracji, kliknij poniższy przycisk, aby zweryfikować swój adres e-mail:</p>
+                        <a href='{verificationLink}' class='button'>Zweryfikuj swój e-mail</a>
+                        <p class='content'>Jeśli nie zakładałeś/aś konta w naszej aplikacji, zignoruj tę wiadomość.</p>
+                    </div>
+                    <div>
+                        <br>
+                        <div class='advice'>
+                        <h4 class='content'>Ważne wskazówki dotyczące bezpieczeństwa:</h4>
+                        <ol>
+                            <li>Zawsze trzymaj dane do Twojego konta w bezpiecznym miejscu.</li>
+                            <li>Nigdy nie ujawniaj nikomu swojego loginu i hasła.</li>
+                            <li>Regularnie zmieniaj swoje hasło.</li>
+                            <li>Jeśli podejrzewasz, że ktoś używa Twojego konta nielegalnie, prosimy o natychmiastowe poinformowanie nas o tym.</li>
+                        </ol>
                         </div>
-                        <div class='content'>
-                          <p>Witaj,</p>
-                          <p>Dziękujemy za utworzenie konta w <strong>[Nazwa Twojej Aplikacji]</strong>. Aby zakończyć proces rejestracji, kliknij poniższy przycisk, aby zweryfikować swój adres e-mail:</p>
-                          <a href='{{verificationLink}}' class='button'>Zweryfikuj swój e-mail</a>
-                          <p>Jeśli nie zakładałeś/aś konta w naszej aplikacji, zignoruj tę wiadomość.</p>
-                        </div>
-                        <div>
-                          <br>
-                          <div class='advice'>
-                            <h4 class='content'>Ważne wskazówki dotyczące bezpieczeństwa:</h4>
-                            <ol>
-                              <li>Zawsze trzymaj dane do Twojego konta w bezpiecznym miejscu.</li>
-                              <li>Nigdy nie ujawniaj nikomu swojego loginu i hasła.</li>
-                              <li>Regularnie zmieniaj swoje hasło.</li>
-                              <li>Jeśli podejrzewasz, że ktoś używa Twojego konta nielegalnie, prosimy o natychmiastowe poinformowanie nas o tym.</li>
-                            </ol>
-                          </div>
-                        </div>
-                        <div class='footer'>
-                          <p>© 2025 [Nazwa Twojej Aplikacji]. Wszelkie prawa zastrzeżone.</p>
-                        </div>
-                      </div>
-                    </body>
-                    </html>"; 
+                    </div>
+                    <div class='footer'>
+                        <p>© 2025 [Nazwa Twojej Aplikacji]. Wszelkie prawa zastrzeżone.</p>
+                    </div>
+                    </div>
+                </body>
+                </html>";
                     await _emailService.SendVerificationEmailAsync(userdto.Email, emailSubject, emailBody);
 
                     return Ok(new { token = result, user = userdto.Email });
                 }
             }
-            return BadRequest(new { message = "Wystąpił błąd w rejestracji." });
-
-            //await _userService.RegisterUser(userdto);
-            //    return Ok();
-                
-            
-
-            //return BadRequest(new { message = "Wystąpił błąd w rejestracji." });
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return BadRequest(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "Wystąpił błąd w rejestracji." });
+            }
+        
         }
 
-        //[HttpPost("verify")]
-        //public async Task<IActionResult> VerifyUserEmail([FromBody] string token)
-        //{
-        //    var user = await _context.Users.FirstOrDefaultAsync(u => u.VerificationToken == request.Token);
-        //    if (user == null)
-        //        return BadRequest("Niepoprawny token");
-
-        //    user.IsVerified = true;
-        //    user.VerificationToken = null; // Token nie jest już potrzebny
-        //    await _context.SaveChangesAsync();
-
-        //    return Ok("Twoje konto zostało zweryfikowane!");
-        //}
+        [HttpPost("verify")]
+        public async Task<IActionResult> VerifyUserEmail(string token)
+        {
+            string result = await _userAccountService.VerifyUserEmail(token);
+            if(result == "Niepoprawny token")
+            {
+                return BadRequest(new { message = result });
+            }
+            else
+            {
+                return Ok("Twoje konto zostało zweryfikowane!");
+            }
+           
+        }
 
 
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
-        {
-            if (ModelState.IsValid)
+        {        
+            try
             {
                 string result = await _userAccountService.Login(loginDto);
-                if (result == "Niepoprawny email lub hasło")
-                {
-                    return BadRequest(new { message = result });
-                }
-                else
-                {
-                    return Ok(new { token = result, user = loginDto.Email });
-                }
+                return Ok(new { token = result, user = loginDto.Email });
             }
-            return BadRequest(new { message = "Wystąpił błąd w rejestracji." });
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return BadRequest(new { message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "Wystąpił błąd w logowaniu." });
+            }
         }
-
-        
     }
 }
