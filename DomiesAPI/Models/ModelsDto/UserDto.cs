5e0f2d177af5ca72994f7d55812379c91f743534
Kasia <kasiazet000@gmail.com>
2025-02-08 23:50:20 +0100

    ErrorMessage created

--- DomiesAPI/Models/ModelsDto/UserDto.cs
+++ DomiesAPI/Models/ModelsDto/UserDto.cs
@@ -1,13 +1,17 @@
-﻿namespace DomiesAPI.Models.ModelsDto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DomiesAPI.Models.ModelsDto
 {
     public class UserDto
     {
+        [Required(ErrorMessage = "Email jest wymagany.")]
         public string Email { get; set; } = null!;
 
+        [Required(ErrorMessage = "Imię jest wymagane.")]
         public string FirstName { get; set; } = null!;
-
+        [Required(ErrorMessage = "Nazwisko jest wymagane.")]
         public string LastName { get; set; } = null!;
-
+        [Required(ErrorMessage = "Hasło jest wymagane.")]
         public string Password { get; set; } = null!;
 
         public int RoleId { get; set; } = 1;
