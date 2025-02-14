57020241860d14456d18f7482cb11adb70331d61
Kasia <kasiazet000@gmail.com>
2025-02-14 19:27:09 +0100

    LoginDto ErrorMessage - Required

--- DomiesAPI/Models/ModelsDto/LoginDto.cs
+++ DomiesAPI/Models/ModelsDto/LoginDto.cs
@@ -1,9 +1,13 @@
-﻿namespace DomiesAPI.Models.ModelsDto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DomiesAPI.Models.ModelsDto
 {
     public class LoginDto
     {
+        [Required(ErrorMessage = "Email jest wymagany.")]
         public string Email { get; set; } = null!;
 
+        [Required(ErrorMessage = "Hasło jest wymagane.")]
         public string Password { get; set; } = null!;
     }
 }
