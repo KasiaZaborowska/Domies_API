b108edfd964f35ba0832a8bb7a3c0150f80b9a78
Kasia <kasiazet000@gmail.com>
2025-02-19 23:40:01 +0100

    validation added to login dto

--- DomiesAPI/Models/ModelsDto/LoginDto.cs
+++ DomiesAPI/Models/ModelsDto/LoginDto.cs
@@ -5,9 +5,11 @@ namespace DomiesAPI.Models.ModelsDto
     public class LoginDto
     {
         [Required(ErrorMessage = "Email jest wymagany.")]
+        [EmailAddress(ErrorMessage = "Błędny format adresu email.")]
         public string Email { get; set; } = null!;
 
         [Required(ErrorMessage = "Hasło jest wymagane.")]
+        [MinLength(8, ErrorMessage = "Hasło musi mieć co najmniej 8 znaków.")]
         public string Password { get; set; } = null!;
     }
 }
