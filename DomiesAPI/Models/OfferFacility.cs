7bae06264497b45f6b5f0ac556112d3ffb075251
Kasia <kasiazet000@gmail.com>
2025-02-20 15:43:55 +0100

    Offer Facility model created

---
+++ DomiesAPI/Models/OfferFacility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DomiesAPI.Models;
+
+public partial class OfferFacility
+{
+    public int OfferId { get; set; }
+
+    public int FacilitieId { get; set; }
+
+    public virtual Facility Facilitie { get; set; } = null!;
+
+    public virtual Offer Offer { get; set; } = null!;
+}
