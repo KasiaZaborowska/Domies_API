1cf213abb1f8b061d2ddbdc745d380b7975ce4da
Kasia <kasiazet000@gmail.com>
2025-02-24 22:02:06 +0100

    phone number with validation

--- DomiesAPI/Models/ModelsDto/UserDto.cs
+++ DomiesAPI/Models/ModelsDto/UserDto.cs
@@ -15,6 +15,11 @@ namespace DomiesAPI.Models.ModelsDto
         [Required(ErrorMessage = "Hasło jest wymagane.")]
         [MinLength(8, ErrorMessage = "Hasło musi mieć co najmniej 8 znaków.")]
         public string Password { get; set; } = null!;
+        
+        [Required(ErrorMessage = "Numer telefonu jest wymagany.")]
+        [StringLength(9, MinimumLength = 9, ErrorMessage = "Numer telefonu musi mieć dokładnie 9 znaków.")]
+        [RegularExpression(@"^\d{9}$", ErrorMessage = "Numer telefonu musi zawierać tylko cyfry.")]
+        public string PhoneNumber { get; set; } = null!;
 
         public int RoleId { get; set; } = 1;
         public string? RoleName { get; set; }
