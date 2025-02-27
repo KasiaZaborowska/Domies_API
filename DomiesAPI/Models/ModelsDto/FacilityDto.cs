8f3024e41b6b18436ff0b451ed268a6e868b91d2
Kasia <kasiazet000@gmail.com>
2025-02-27 23:36:26 +0100

    FacilityDto created

---
+++ DomiesAPI/Models/ModelsDto/FacilityDto.cs
@@ -0,0 +1,12 @@
+﻿namespace DomiesAPI.Models
+{
+    public class FacilityDto
+    {
+        public int Id { get; set; }
+
+        public string FacilitiesType { get; set; } = null!;
+
+        public string FacilitiesDescription { get; set; } = null!;
+        //public virtual ICollection<Offer> Offers { get; set; } = new List<Offer>();
+    }
+}
