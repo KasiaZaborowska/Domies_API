4ee6b73eb1f5722c7754cc03a660dcacb4b3eb26
Kasia <kasiazet000@gmail.com>
2025-02-27 23:35:33 +0100

    ICollection<Offer> added

--- DomiesAPI/Models/Facility.cs
+++ DomiesAPI/Models/Facility.cs
@@ -7,5 +7,6 @@
         public string FacilitiesType { get; set; } = null!;
 
         public string FacilitiesDescription { get; set; } = null!;
+        public virtual ICollection<Offer> Offers { get; set; } = new List<Offer>();
     }
 }
