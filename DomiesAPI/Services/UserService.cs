a6eb6586e49af9b20f314acc7362c6c790277102
Kasia <kasiazet000@gmail.com>
2025-02-28 20:52:00 +0100

    promotion to manager role and downgrade to user role and deleteUser fixed

--- DomiesAPI/Services/UserService.cs
+++ DomiesAPI/Services/UserService.cs
@@ -9,10 +9,11 @@ namespace DomiesAPI.Services
     {
         Task<List<UserDto>> GetUsers(String userEmail);
         Task<UserDto> GetUserById(string email, String userEmail);
-        Task<string> CreateUser(UserDto userDto, String userEmail);
-        Task<string> UpdateUser(string email, UserDto userDto, String userEmail);
+        Task<string> ChangeRole(string email, int newRole, String userEmail);
+        //Task<string> CreateUser(UserDto userDto, String userEmail);
+        //Task<string> UpdateUser(string email, UserDto userDto, String userEmail);
         // Task<ApplicationDto> UpdateApplication(int id, ApplicationDto applicationDto);
-        //Task<bool> DeleteUserById(int id, String userEmail);
+        Task<bool> DeleteUserById(string email, String userEmail);
     }
 
     public class UserService : IUserService
@@ -83,37 +84,67 @@ namespace DomiesAPI.Services
             }
         }
 
-        public async Task<string> CreateUser(UserDto userDto, String userEmail)
-        {
-            using var transaction = await _context.Database.BeginTransactionAsync();
+        //public async Task<string> CreateUser(UserDto userDto, String userEmail)
+        //{
+        //    using var transaction = await _context.Database.BeginTransactionAsync();
 
-            try
-            {
-                var userEntity = new User
-                {
-                    Email = userDto.Email,
-                    FirstName = userDto.FirstName,
-                    LastName = userDto.LastName,
-                    PhoneNumber = userDto.PhoneNumber,
-                    RoleId = userDto.RoleId,
-                    DateAdd = DateTime.Now,
-                };
+        //    try
+        //    {
+        //        var userEntity = new User
+        //        {
+        //            Email = userDto.Email,
+        //            FirstName = userDto.FirstName,
+        //            LastName = userDto.LastName,
+        //            PhoneNumber = userDto.PhoneNumber,
+        //            RoleId = userDto.RoleId,
+        //            DateAdd = DateTime.Now,
+        //        };
 
 
-                _context.Users.Add(userEntity);
-                await _context.SaveChangesAsync();
-                await transaction.CommitAsync();
+        //        _context.Users.Add(userEntity);
+        //        await _context.SaveChangesAsync();
+        //        await transaction.CommitAsync();
 
-                return "Stworzono aplikację.";
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Wystąpił błąd: {ex.Message}");
-                throw new ApplicationException("Błąd podczas tworzenia aplikacji", ex);
-            }
-        }
+        //        return "Stworzono aplikację.";
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine($"Wystąpił błąd: {ex.Message}");
+        //        throw new ApplicationException("Błąd podczas tworzenia aplikacji", ex);
+        //    }
+        //}
 
-        public async Task<string> UpdateUser(string email, UserDto userDto, String userEmail)
+        //public async Task<string> UpdateUser(string email, UserDto userDto, String userEmail)
+        //{
+        //    try
+        //    {
+        //        var userEntity = await _context.Users
+        //            .FirstOrDefaultAsync(u => u.Email == email);
+
+        //        if (userEntity == null)
+        //        {
+        //            return null;
+        //        }
+
+        //        userEntity.Email = userDto.Email;
+        //        userEntity.FirstName = userDto.FirstName;
+        //        userEntity.LastName = userDto.LastName;
+        //        userEntity.PhoneNumber = userDto.PhoneNumber;
+        //        userEntity.RoleId = userDto.RoleId;
+
+        //        await _context.SaveChangesAsync();
+
+        //        return "Edytowano.";
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine($"Wystąpił błąd: {ex.Message}");
+        //        throw new ApplicationException("Błąd podczas edytowania oferty", ex);
+        //    }
+        //}
+
+
+        public async Task<string> ChangeRole(string email, int newRole, String userEmail)
         {
             try
             {
@@ -122,50 +153,67 @@ namespace DomiesAPI.Services
 
                 if (userEntity == null)
                 {
-                    return null;
+                    return "Nie ma takiego użytkownika.";
                 }
 
-                userEntity.Email = userDto.Email;
-                userEntity.FirstName = userDto.FirstName;
-                userEntity.LastName = userDto.LastName;
-                userEntity.PhoneNumber = userDto.PhoneNumber;
-                userEntity.RoleId = userDto.RoleId;
+                userEntity.RoleId = newRole;
 
                 await _context.SaveChangesAsync();
 
-                return "Edytowano.";
+                return "Zmieniono rolę.";
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Wystąpił błąd: {ex.Message}");
-                throw new ApplicationException("Błąd podczas edytowania oferty", ex);
+                throw new ApplicationException("Błąd podczas zmiany roli", ex);
             }
         }
 
-        //public async Task<bool> DeleteUserById(int id, String userEmail)
-        //{
-        //    try
-        //    {
-        //        var applicationToDelete = await _context.Applications
-        //             //.Include(o => o.Address)
-        //             .Where(a => a.Applicant == userEmail)
-        //             .FirstOrDefaultAsync(o => o.Id == id);
+        public async Task<bool> DeleteUserById(string email, String userEmail)
+        {
+            try
+            {
+                var userToDelete = await _context.Users
+                     //.Include(o => o.Address)
+                     .Where(a => a.Email == email)
+                     .FirstOrDefaultAsync(o => o.Email == email);
 
-        //        if (applicationToDelete == null)
-        //        {
-        //            return false;
-        //        }
-        //        _context.Applications.Remove(applicationToDelete);
-        //        await _context.SaveChangesAsync();
+                if (userToDelete == null)
+                {
+                    return false;
+                }
+                using var transaction = await _context.Database.BeginTransactionAsync();
 
-        //        return true;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine($"Wystąpił błąd: {ex.Message}");
-        //        throw new ApplicationException("Błąd podczas pobierania szczegółowych informacji", ex);
-        //    }
-        //}
+                try
+                {
+                    var animalsToDelete = _context.Animals.Where(p => p.Owner == email).ToList();
+
+                    _context.Animals.RemoveRange(animalsToDelete);
+
+                    _context.SaveChanges();
+
+                    _context.Users.Remove(userToDelete);
+                    await _context.SaveChangesAsync();
+
+  
+                    await transaction.CommitAsync();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    await transaction.RollbackAsync();
+                    return false;
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Wystąpił błąd: {ex.Message}");
+                throw new ApplicationException("Błąd podczas pobierania szczegółowych informacji", ex);
+            }
+        }
 
     }
 
